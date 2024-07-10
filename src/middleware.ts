838019c188aed89c6d8ef8d33de2efbb828f5c4c
Saad Nauman <87758531+saadings@users.noreply.github.com>
2024-07-11 00:44:30 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   if (!request.cookies.get("appwrite-session")?.value) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+    return NextResponse.rewrite(new URL("/sign-in", request.url));
   }
 
   return NextResponse.next();
